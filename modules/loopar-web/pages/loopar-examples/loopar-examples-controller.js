07244d168a95b91f6ba0dcf250dd8820bdecde38
alphabit-technology <qubitcore.io@gmail.com>
2026-05-14 23:41:14 -0400

    Update to version 2.0.39

---
+++ modules/loopar-web/pages/loopar-examples/loopar-examples-controller.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+import {PageController} from 'loopar';
+
+export default class LooparExamplesController extends PageController {
+    constructor(props){
+        super(props);
+    }
+}
---
+++ modules/loopar-webpage/pages/loopar-authentication/loopar-authentication-controller.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+import {PageController} from 'loopar';
+
+export default class LooparAuthenticationController extends PageController {
+    constructor(props){
+        super(props);
+    }
+}
